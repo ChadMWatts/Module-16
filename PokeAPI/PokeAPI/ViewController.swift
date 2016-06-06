4af7a88a98454b48adc058dc13c22f10e206eeed
ChadMWatts <chadmwatts@gmail.com>
2016-06-06 14:04:16 -0600

    inital Commit

---
+++ PokeAPI/PokeAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokeAPI
+//
+//  Created by Chad Watts on 6/6/16.
+//  Copyright © 2016 Chad Watts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
